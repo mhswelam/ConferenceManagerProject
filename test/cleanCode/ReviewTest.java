caeee7560e31452f7209bbe9ee6029e5aef932f9
Polina Kud <polinak@uw.edu>
2014-05-21 15:39:35 -0700

    Added Review test class.

---
+++ test/cleanCode/ReviewTest.java
@@ -0,0 +1,14 @@
+package cleanCode;
+
+import org.junit.Test;
+
+public class ReviewTest {
+
+	/**
+	 * Tests the id validity of a review.
+	 */
+	@Test
+	public void testGeMyReviewID() {
+		
+	}
+}
