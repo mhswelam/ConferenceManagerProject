0fa8ef6354fa35445c3c1eaf3b4d0135307b513a
Polina Kud <polinak@uw.edu>
2014-05-21 20:41:43 -0700

    Experimenting with Windowbuilder.

---
+++ src/cleanCode/UI_Window.java
@@ -0,0 +1,43 @@
+package cleanCode;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class UI_Window {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					UI_Window window = new UI_Window();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public UI_Window() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
