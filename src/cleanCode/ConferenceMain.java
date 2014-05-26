407e644f366267756e3e58611da692f42839d4e9
Polina Kud <polinak@uw.edu>
2014-05-26 14:15:24 -0700

    Added ConferenceMain.

---
+++ src/cleanCode/ConferenceMain.java
@@ -0,0 +1,41 @@
+package cleanCode;
+
+import java.awt.EventQueue;
+
+/**
+ * @author Clean Code
+ * @author Polina Kud
+ * Runs Conference by instantiating UI_Page.
+ *
+ */
+public class ConferenceMain {
+	
+	/**
+	 * Private constructor prevents main class from instantiating.
+	 */
+	private ConferenceMain() {
+		throw new IllegalStateException();
+	}
+
+	/**
+	 * Invokes UI_Page.
+     * Command line arguments are ignored.
+     * 
+     * @param theArgs command line arguments.
+	 */
+	public static void main(String... theArgs) {
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+            	try {
+					UI_Page window = new UI_Page();
+					window.start();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+            }
+        });
+
+	}
+
+}
