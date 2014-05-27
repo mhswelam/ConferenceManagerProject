3775ca0e4183ef623bcaa74dafdbdda63fcb6040
Polina Kud <polinak@uw.edu>
2014-05-27 10:42:16 -0700

    Still working on UI.

--- src/cleanCode/UI_Login.java
+++ src/cleanCode/UI_Login.java
@@ -1,9 +1,41 @@
 package cleanCode;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+
+import javax.swing.JPanel;
+
 /**
  * @author Clean Code
  * This class to create a user interface for the login
  *
  */
-public class UI_Login {
+public class UI_Login extends JPanel {
+	
+	/**
+	 * Instantiates Log In panel when Conference starts running.
+	 */
+	public UI_Login() {
+		super(new BorderLayout());
+		setPreferredSize(new Dimension(300, 400));
+		setBackground(new Color(205, 205, 205));
+	}
+	
+	public void logIn() {
+//		JPanel mainWindow = new JPanel(new BorderLayout());
+//		mainWindow.setBackground(new Color(205, 205, 205));
+//		mainWindow.setPreferredSize(new Dimension(300, 400));
+		
+//		add(mainWindow, BorderLayout.CENTER);
 
+		
+//		Object[] choices = {"Author", "Reviewer", "SubProgram Chair", "Program Chair"};
+//		JLabel logIn = new JLabel("Log in", JLabel.CENTER);
+//		logIn.setVerticalTextPosition(JLabel.CENTER);
+//		logIn.setHorizontalTextPosition(JLabel.CENTER);
+//		add(logIn, BorderLayout.CENTER);
+		
+//		JOptionPane.showOptionDialog(this ,"Log In", "Log In", JOptionPane.YES_OPTION, JOptionPane.PLAIN_MESSAGE, null, choices, choices[0]);
+	}
 }
--- src/cleanCode/UI_Page.java
+++ src/cleanCode/UI_Page.java
@@ -1,7 +1,8 @@
 package cleanCode;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.FlowLayout;
+import java.awt.Dimension;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -13,23 +14,35 @@ import javax.swing.JPanel;
  */
 public class UI_Page {
 	/** Main window frame. */
+<<<<<<< Upstream, based on origin/master
 	public JFrame myFrame;
 	
 	/**
 	 * To hold the conference class
 	 */
 	public Conference myConference;
+=======
+	private JFrame myFrame;
+	UI_Login logInPanel;
+	
+>>>>>>> 77a39c7 Still working on UI.
 	//Observes all other UI classes.
 	//Controls changes to all other UI classes.
 
 	/**
 	 * Constructs graphical user interface.
 	 */
+<<<<<<< Upstream, based on origin/master
 	public UI_Page(Conference aConference) {
 		myFrame = new JFrame();
 		myFrame.getContentPane().setBackground(Color.WHITE);
 		myFrame.setBackground(Color.WHITE);
 		myConference = aConference;
+=======
+	public UI_Page() {
+		myFrame = new JFrame("Conference");
+		logInPanel = new UI_Login();
+>>>>>>> 77a39c7 Still working on UI.
 	}
 
 	/**
@@ -37,13 +50,54 @@ public class UI_Page {
 	 */
 	public void start() {
 		myFrame.setVisible(true);
-		myFrame.getContentPane().setBackground(Color.WHITE);
-		myFrame.getContentPane().setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
-		
-		JPanel contentPane = new JPanel();
-		myFrame.getContentPane().add(contentPane);
-		//700 width 500 height
-		myFrame.setBounds(100, 100, 1000, 700);
 		myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//Size of the frame 1200 width 800 height
+		myFrame.setPreferredSize(new Dimension(1200, 900));
+		
+		
+		setUpPanels();
+		
+		myFrame.pack();
+        myFrame.setLocationRelativeTo(null);
+        logInPanel.logIn();
+	}
+	
+	/**
+	 * Sets up all interface panels.
+	 */
+	private void setUpPanels() {
+		UI_ControlPanel controlPanel = new UI_ControlPanel();
+		UI_PaperInfo paperInfo = new UI_PaperInfo();
+		
+		UI_TaskInfo taskInfo  = new UI_TaskInfo();
+		UI_UserInfo userInfo = new UI_UserInfo();
+		
+		JPanel northPanel = new JPanel();
+		northPanel.setPreferredSize(new Dimension(200, 100));
+		northPanel.setBackground(new Color(105, 105, 105));
+		
+		JPanel centerPanel = new JPanel(new BorderLayout());
+		centerPanel.setPreferredSize(new Dimension(1000, 700));
+		centerPanel.setBackground(new Color(255, 255, 255));
+		centerPanel.add(logInPanel, BorderLayout.CENTER);
+		
+		JPanel eastPanel = new JPanel();
+		eastPanel.setPreferredSize(new Dimension(100, 100));
+		eastPanel.setBackground(new Color(105, 105, 105));
+		
+		JPanel westPanel = new JPanel();
+		westPanel.setPreferredSize(new Dimension(100, 100));
+		westPanel.setBackground(new Color(105, 105, 105));
+		
+		JPanel southPanel = new JPanel();
+		southPanel.setPreferredSize(new Dimension(200, 100));
+		southPanel.setBackground(new Color(105, 105, 105));
+		
+		
+		myFrame.add(northPanel, BorderLayout.NORTH);
+		myFrame.add(eastPanel, BorderLayout.EAST);
+		myFrame.add(westPanel, BorderLayout.WEST);
+		myFrame.add(southPanel, BorderLayout.SOUTH);
+		myFrame.add(logInPanel, BorderLayout.CENTER);
 	}
 }
